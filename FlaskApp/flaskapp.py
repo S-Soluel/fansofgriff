7c8eebaaf018a3d29d9fd32d4d7f2e5ab83a7fd5
Joe Barnard <joe.barnard@drake.edu>
2024-02-26 18:43:08 -0600

    Updated display text

--- FlaskApp/flaskapp.py
+++ FlaskApp/flaskapp.py
@@ -10,7 +10,7 @@ def hello():
     loop = asyncio.new_event_loop()
     location = loop.run_until_complete(PyTile())
     loop.close()
-    return f'<h1>Hello from Flask! The dog is at: {location}</h1>'
+    return f'<h1>Hello from Flask! The Tile Tracker is at: {location}</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
