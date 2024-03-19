61b6afca64ba38715d200d8b416ec8f5fea2714b
Sam Solheim <sam.solheim@drake.edu>
2024-03-18 20:17:31 -0500

    Adding template info

--- FlaskApp/email_verification.py
+++ FlaskApp/email_verification.py
@@ -3,8 +3,14 @@ import boto3
 # Create SES client
 ses = boto3.client('ses')
 
-response = ses.verify_email_identity(
-  EmailAddress = 'sam.solheim@drake.edu'
+
+#response = ses.verify_email_identity(
+#  EmailAddress = 'sam.solheim@drake.edu'
+#)
+
+response = ses.send_custom_verification_email(
+    EmailAddress = 'samsolheim06@gmail.com', 
+    TemplateName = 'EmailVerification'
 )
 
 print(response)
