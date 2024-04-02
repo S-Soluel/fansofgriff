c6e3e408103b0caa8be57a93a1d16757eb66ba2a
Joe Barnard <joe.barnard@drake.edu>
2024-04-01 22:22:16 -0500

    added ability ro refresh data

--- FlaskApp/flaskapp.py
+++ FlaskApp/flaskapp.py
@@ -1,46 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from pytileTester import main as PyTile
 from calcmapsize import calc_map_size_from_desired_width as calcheight
-from turbo_flask import Turbo
 import asyncio, time, random
 import folium
-import threading
-
 # ------------------------------------
 
-app = Flask(__name__)
-turbo = Turbo(app)
+def get_tile_data():
+    # Simulate fetching tile data synchronously
+    return 41.603062, -93.653819
 
-def update_load():
-    while True:
-        try:
-            with app.app_context():
-                print("test")
-                turbo.push(turbo.replace(render_template('loadmap.html'), 'load'))
-        except Exception as e:
-            print(f"Error in update_load: {str(e)}")
-        time.sleep(5)
-    
-events = [
-    {
-        'todo' : 'Relays Opening',
-        'date' : '04-26-2024',
-    },
-    {
-        'todo' : 'Beautiful Bulldog Contest',
-        'date' : '04-20-2024'
-    }
-]
+griff_icon = "https://griffcapstone.s3.us-east-2.amazonaws.com/images.png"
+lat_a, long_a = 41.607283, -93.659769  # Top left corner of campus
+lat_b, long_b = 41.600262, -93.649731  # Bottom right corner of campus
+lat_center_coord = (lat_a + lat_b) / 2
+long_center_coord = (long_a + long_b) / 2
+map_center_coords = ((lat_a + lat_b) / 2, (long_a + long_b) / 2)
+desired_width = 1000     # width of map on webpage, used to calc the height
+desired_height = -1 * calcheight(lat_a, long_a, lat_b, long_b, desired_width)
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    griff_icon = "https://griffcapstone.s3.us-east-2.amazonaws.com/images.png"
-    lat_a, long_a = 41.607283, -93.659769  # Top left corner of campus
-    lat_b, long_b = 41.600262, -93.649731  # Bottom right corner of campus
-    map_center_coords = ((lat_a + lat_b) / 2, (long_a + long_b) / 2)
-
-    desired_width = 1000     # width of map on webpage, used to calc the height
-    desired_height = -1 * calcheight(lat_a, long_a, lat_b, long_b, desired_width)
 
     loop = asyncio.new_event_loop()
     lat, long = loop.run_until_complete(PyTile())
@@ -66,32 +47,33 @@ def hello():
     m.get_root().height = f"{desired_height}px"
     iframe = m.get_root()._repr_html_()
 
-    return render_template('landing.html', lat=lat, long=long, iframe=iframe)
-
-def get_tile_data():
-    # Simulate fetching tile data synchronously
-    return 41.603062, -93.653819
-
-@app.route('/')
-def home():
     return render_template('landing.html')
+
+@app.route('/map_data', methods = ['GET'])
+def map_data():
+    loop = asyncio.new_event_loop()
+    lat, long = loop.run_until_complete(PyTile())
+    loop.close()
+
+    data = {'latitude': lat_center_coord, 
+            'longitude': long_center_coord,
+            'desiredw': desired_width, 
+            'desiredh': desired_height,
+            'markerlat': lat,
+            'markerlong': long,
+            'lat_a': lat_a,
+            'lat_b': lat_b,
+            'long_a': long_a,
+            'long_b': long_b,
+
+    }
+    return jsonify(data)
 
 # Define route for the calendar page
 @app.route('/calendar')
 def calendar():
     # Add logic to render the calendar page
     return render_template('calendar.html')
-
-@app.context_processor
-def inject_load():
-    loop = asyncio.new_event_loop()
-    lat, long = loop.run_until_complete(PyTile())
-    loop.close()
-    lat2 = random.randrange(100) + lat
-    long2 = random.randrange(100) + long
-    print(lat2)
-    print(long2)
-    return {'lat2': lat2, 'long2': long2}
 
 # Define route for the email notifications page
 @app.route('/notifications')
@@ -101,4 +83,3 @@ def notifications():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-    threading.Thread(target=update_load).start()
